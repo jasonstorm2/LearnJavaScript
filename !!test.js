1c7611fe9f3217f6f3029876313c3f05c59a8490
jason <json329731049@gmail.com>
2019-03-21 20:38:41 +0800

    a

---
+++ !!test.js
@@ -0,0 +1,13 @@
+var a;
+
+console.log("a的值："+a);
+console.log("!a:"+!a);
+console.log("!!a:"+!!a);
+console.log("--------------------");
+
+
+var b = 0;
+console.log("b的值："+b);
+console.log("!b:"+!b);
+console.log("!!b:"+!!b);
+
